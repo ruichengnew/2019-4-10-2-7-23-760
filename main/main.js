1da682ebc230a0eb908e173bb3d40505131e1233
rui.cheng <rui.cheng@17zuoye.com>
2019-05-28 18:37:40 +0800

    finish code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    let price = 0;
+    if (inputs.distance < 2) {
+        price += 6 + 0.25 * inputs.parkTime;
+    } else if (inputs.distance >= 2 && inputs.distance < 8) {
+        price += 6 + (inputs.distance - 2) * 0.8 + 0.25 * inputs.parkTime;
+    } else {
+        price +=
+            6 +
+            6 * 0.8 +
+            (inputs.distance - 8) * (0.8 * (1 + 0.5)) +
+            0.25 * inputs.parkTime;
+    }
+    return parseInt(price.toFixed(0));
 };
